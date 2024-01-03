f819bc8bdb62e105453d9dd7bdd15c69b577df8e
gustn1029 <gustn1029@gmail.com>
2024-01-03 11:32:14 +0900

    npm install netlify-cli -gg

--- src/components/form/FormButton.tsx
+++ src/components/form/FormButton.tsx
@@ -1,11 +1,12 @@
 import classNames from "classnames";
-import React from "react";
+import React, { KeyboardEvent } from "react";
 
 interface BProps {
   className?: string;
   text?: string;
   type?: "button" | "submit";
   isDelete?: boolean;
+  onKeyDown?: (e:KeyboardEvent<HTMLButtonElement>) => void
   [x: string]: any;
 }
 
@@ -14,10 +15,11 @@ const FormButton = ({
   className = "",
   text = "",
   isDelete = false,
+  onKeyDown,
   ...restProps
 }: BProps) => {
   return (
-    <button type={type} className={classNames(className, `w-[100px] shadow-md rounded-lg font-semibold border ${isDelete ? 'border-red-300 text-red-500 hover:bg-red-500': 'border-slate-300 text-slate-500 hover:bg-slate-500'}  hover:text-white`)} {...restProps}>
+    <button type={type} onKeyDown={onKeyDown} className={classNames(className, `w-[100px] shadow-md rounded-lg font-semibold border ${isDelete ? 'border-red-300 text-red-500 hover:bg-red-500': 'border-slate-300 text-slate-500 hover:bg-slate-500'}  hover:text-white`)} {...restProps}>
       {text}
     </button>
   );
--- src/components/layout/TodoClient.tsx
+++ src/components/layout/TodoClient.tsx
@@ -1,53 +1,64 @@
-'use client';
-import React, { useState } from 'react'
-import FormInput from '../form/FormInput';
-import FormButton from '../form/FormButton';
-import { toast } from 'react-toastify';
-import TodoSkeleton from '../todo/TodoSkeleton';
-import TodoList from '../todo/TodoList';
-import { TodoData } from '@/type';
+"use client";
+import React, { KeyboardEvent, useState } from "react";
+import FormInput from "../form/FormInput";
+import FormButton from "../form/FormButton";
+import { toast } from "react-toastify";
+import TodoList from "../todo/TodoList";
+import { TodoData } from "@/type";
 
 const TodoClient = () => {
-  const [inputValue, setInputValue] = useState('')
-  const [todoList, setTodoList] = useState<TodoData[]>([])
-  const pushTodoHandler = ()=> {
-    const todoListData: TodoData = 
-      {
-        todo: inputValue,
-        id: `todoList${todoList.length}`
-      }
-    
-    setTodoList([...todoList, todoListData]);
-    setInputValue('');
-  }
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState<TodoData[]>([]);
 
-  const todoDeleteHandler = (id:string) => {
-    const updateTodoList = todoList.filter(el => el.id !== id);
+  const pushTodoHandler = () => {
+    if (inputValue === "") {
+      alert('할 일을 입력해주세요.')
+      return;
+    }
+    const todoListData: TodoData = {
+      todo: inputValue,
+      id: `todoList${todoList.length}`,
+    };
+
+    setTodoList([...todoList, todoListData]);
+    setInputValue("");
+  };
+
+  const keyDownHandler = (e: KeyboardEvent<HTMLButtonElement>) => {
+    if (e.key === "Enter") {
+      pushTodoHandler();
+    }
+  };
+
+  const todoDeleteHandler = (id: string) => {
+    const updateTodoList = todoList.filter((el) => el.id !== id);
     updateTodoList.forEach((el, idx) => {
       el.id = `todoList${idx}`;
-    })
+    });
 
     setTodoList(updateTodoList);
-  }
+  };
+
   return (
-    <section className='max-w-[600px] w-full mx-auto'>
-      <article className='flex w-full mb-[60px] gap-x-5 justify-center'>
+    <section className="max-w-[600px] w-full mx-auto">
+      <article className="flex w-full mb-[60px] gap-x-5 justify-center">
         <FormInput
-          name='등록'
+          name="등록"
           inputValue={inputValue}
-          onChange={(e)=>setInputValue(e.target.value)}
+          onChange={(e) => setInputValue(e.target.value)}
           isHidden={true}
-          placeHolder='등록할 일정을 입력하세요.'
-          divClassName='max-w-[500px] w-full flex-1'
-          inputClassName='block w-full border border-slate-300 p-3 rounded-lg'
+          placeHolder="등록할 일정을 입력하세요."
+          divClassName="max-w-[500px] w-full flex-1"
+          inputClassName="block w-full border border-slate-300 p-3 rounded-lg"
+          onKeyDown={keyDownHandler}
         />
-        <FormButton text='등록' className='' onClick={pushTodoHandler} />
+        <FormButton text="등록" className="" onClick={pushTodoHandler} />
       </article>
       <article>
-          <TodoList onDelete={todoDeleteHandler} todoList={todoList} />
+        <TodoList onDelete={todoDeleteHandler} todoList={todoList} />
       </article>
     </section>
-  )
-}
+  );
+};
 
-export default TodoClient
+export default TodoClient;
